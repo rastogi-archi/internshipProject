efe722bb33dff26dec938afd534f7596bf4073e6
rastogi-archi <rastogiarchi18@gmail.com>
2025-03-15 15:20:28 +0530

    checkAuth component added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import AddNewCoupon from "./pages/coupon/AddNewCoupon";
 import UserHome from "./pages/user/UserHome";
 import Navbar from "./components/Navbar";
 import EditCoupon from "./pages/coupon/EditCoupon";
+import CheckAuth from "./components/CheckAuth"
 
 function App() {
   const location = useLocation();
@@ -17,12 +18,36 @@ function App() {
       {location.pathname !== "/login" && location.pathname !== "/register" && <Navbar />}
 
       <Routes>
-        <Route path="/" element={<UserHome />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/admin" element={<AdminHome />} />
-        <Route path="/admin/add" element={<AddNewCoupon />} />
-        <Route path="/admin/update/:id" element={<EditCoupon />} />
+        <Route path="/" element={
+          <CheckAuth>
+            <UserHome />
+          </CheckAuth>
+        } />
+        <Route path="/login" element={
+          <CheckAuth>
+            <Login />
+          </CheckAuth>
+        } />
+        <Route path="/register" element={
+          <CheckAuth>
+            <Register />
+          </CheckAuth>
+        } />
+        <Route path="/admin" element={
+          <CheckAuth>
+            <AdminHome />
+          </CheckAuth>
+        } />
+        <Route path="/admin/add" element={
+          <CheckAuth>
+            <AddNewCoupon />
+          </CheckAuth>
+        } />
+        <Route path="/admin/update/:id" element={
+          <CheckAuth>
+            <EditCoupon />
+          </CheckAuth>
+        } />
       </Routes>
 
       <Toaster />
--- server/routes/Coupon.routes.js
+++ server/routes/Coupon.routes.js
@@ -1,13 +1,13 @@
 import express from "express";
 import { addNewCoupon, claimCoupon, deleteCoupon, getAllCoupons, getCouponById, updateCoupon, updateCouponStatus } from "../controllers/Coupon.controllers.js";
-import { authMiddleware } from "../middlewares/Auth.middlewares.js";
+// import { authMiddleware } from "../middlewares/Auth.middlewares.js";
 
 const router = express.Router();
 
 router.post("/admin/add", addNewCoupon);
 router.get("/all", getAllCoupons);
 router.get("/coupon/getCouponById/:id", getCouponById)
-router.post("/claim", authMiddleware,claimCoupon);
+router.post("/claim",claimCoupon);
 router.delete("/admin/delete/:id", deleteCoupon);
 router.put("/admin/toggleStatus/:id", updateCouponStatus);
 router.put("/admin/update/:id", updateCoupon);
